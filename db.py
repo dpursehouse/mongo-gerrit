27705cdc0cb35fd2898f272df041de1864b3eb76
David Pursehouse <david.pursehouse@gmail.com>
2016-08-20 14:35:28 +0900

    Move database class to a separate module
    Change-Id: Id0f3ba98b561c17bc790c3dd08bb289a6ff6e7bb

---
+++ db.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient, ASCENDING
+
+
+class GerritMongoDatabase:
+    def __init__(self, name):
+        client = MongoClient()
+        self.name = name
+        db = client[self.name]
+        self.changes = db.changes
+        self.meta = db.meta
+        self.changes.create_index([('id', ASCENDING)], unique=True)
+        self.meta.create_index([('name', ASCENDING)], unique=True)
+
+    def get_last_update(self):
+        metadata = self.meta.find_one({'name': self.name})
+        if metadata:
+            return metadata['last-update']
+        return None
+
+    def set_last_update(self, timestamp):
+        update_filter = {'name': self.name}
+        update = {'name': self.name, 'last-update': timestamp}
+        self.meta.replace_one(update_filter, update, upsert=True)
+
+    def update_change(self, change):
+        self.changes.replace_one(
+            {'id': change['id']},
+            change,
+            bypass_document_validation=True,
+            upsert=True)
+
+    def change_count(self):
+        return self.changes.count()
