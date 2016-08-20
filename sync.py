f7f7e4c7f70be4da5a6fc57845fb9b9c777ba8f2
David Pursehouse <david.pursehouse@gmail.com>
2016-08-20 18:12:41 +0900

    Pass the site as an argument and get settings from mongo-gerrit.ini
    Change-Id: I9485236bcbb34468ad956fee65a6b387625f9d17

--- sync.py
+++ sync.py
@@ -1,20 +1,44 @@
 from __future__ import print_function
 
 import datetime
+import sys
+from argparse import ArgumentParser
+from ConfigParser import ConfigParser
+from os.path import expanduser
 
 from pygerrit2.rest import GerritRestAPI
-from pygerrit2.rest.auth import HTTPBasicAuthFromNetrc
+from pygerrit2.rest.auth import HTTPBasicAuthFromNetrc, HTTPDigestAuthFromNetrc
 
 from db import GerritMongoDatabase
 
 
 ALL_OPTIONS = ['DETAILED_LABELS', 'ALL_REVISIONS', 'ALL_COMMITS',
                'DETAILED_ACCOUNTS', 'MESSAGES', 'COMMIT_FOOTERS']
+AUTH_METHODS = {
+    'basic': HTTPBasicAuthFromNetrc,
+    'digest': HTTPDigestAuthFromNetrc
+}
 
-url = 'https://gerrit-review.googlesource.com'
-auth = HTTPBasicAuthFromNetrc(url=url)
-gerrit = GerritRestAPI(url=url, auth=auth)
-db = GerritMongoDatabase(name='gerrit-review')
+config = ConfigParser()
+config.read(['mongo-gerrit.ini', expanduser('~/mongo-gerrit.ini')])
+
+parser = ArgumentParser()
+parser.add_argument("site", help="Name of the Gerrit site to sync")
+args = parser.parse_args()
+
+site = args.site
+if not config.has_section(site):
+    print("no config for site %s" % site, file=sys.stderr)
+    exit(1)
+
+url = config.get(site, 'url')
+auth = config.get(site, 'auth')
+if auth not in AUTH_METHODS:
+    print("invalid authentication method %s" % auth, file=sys.stderr)
+    exit(1)
+
+gerrit = GerritRestAPI(url=url, auth=AUTH_METHODS[auth](url=url))
+db = GerritMongoDatabase(name=site)
 
 last_update = db.get_last_update()
 if last_update:
